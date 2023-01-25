46adfe4afdb0f97b02eeaa09974fcf222c3e99bf
Francisco Giordano <fg@frang.io>
2023-01-25 18:46:11 -0300

    exclude eof from tokens

--- src/lex.ts
+++ src/lex.ts
@@ -15,8 +15,10 @@ interface TokenCommon {
   utf8Length: number;
 }
 
-type Token = TokenCommon & { kind: Exclude<Kind, 'delim'> }
+type Token = TokenCommon & { kind: Exclude<Kind, 'delim' | 'eof'> }
            | TokenCommon & { kind: 'delim', side: Side };
+
+type TokenOrEOF = Token | { kind: 'eof' };
 
 export function lex(source: string): Token[] {
   const res = [];
@@ -40,11 +42,13 @@ export function lex(source: string): Token[] {
 
     const t = makeToken(m, utf8Offset);
 
-    res.push(t);
-
     if (t.kind === 'eof') {
       break;
-    } else if (t.kind === 'string' || t.kind === 'comment' || t.kind === 'pragmatoken') {
+    }
+
+    res.push(t);
+
+    if (t.kind === 'string' || t.kind === 'comment' || t.kind === 'pragmatoken') {
       utf8Offset += t.utf8Length - t.value.length;
     } else if (t.kind === 'keyword') {
       if (t.value === 'pragma') {
@@ -66,7 +70,7 @@ interface RegExpMatch {
   groups?: { [key: string]: string };
 }
 
-const makeToken = (m: RegExpMatch, utf8Offset: number): Token => {
+const makeToken = (m: RegExpMatch, utf8Offset: number): TokenOrEOF => {
   const g = m.groups!;
   const kind = kinds.find(k => g[k] !== undefined);
   if (kind === undefined) {
@@ -78,5 +82,5 @@ const makeToken = (m: RegExpMatch, utf8Offset: number): Token => {
   const utf8Length = getUtf8Length(value);
   const side = kind !== 'delim' ? undefined : g.rdelim !== undefined ? 'right' : 'left';
   const t: TokenCommon = { kind, value, start, utf8Start, utf8Length, side };
-  return t as Token;
+  return t as TokenOrEOF;
 };
